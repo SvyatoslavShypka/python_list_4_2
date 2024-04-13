2698fc3a98393c8e6ba2aec9f7bdef49bb667025
Svyatoslav Shypka <svyat200909@gmail.com>
2024-04-13 22:12:13 +0200

    4.3 todo2

--- lab_4_3tail.py
+++ lab_4_3tail.py
@@ -7,61 +7,96 @@ def tail(file, lines=None, bytes=None):
         lines = 10
 
     if file:
+        print("file: " + file)
         try:
             with open(file, 'rb') as f:
                 if bytes is not None:
                     f.seek(-int(bytes), 2)
                 lines_to_print = f.readlines()
+                print(lines_to_print)
+                decoded = []
+                for line in lines_to_print:
+                    decoded.append(line.decode("utf-8"))
+                print(decoded)
+                lines_all = "".join(decoded)
+                lines_to_print = lines_all
         except FileNotFoundError:
             print("File not found:", file)
             sys.exit(1)
     else:
+        print("else")
         lines_to_print = sys.stdin.readlines()
+        print(lines_to_print)
+        if bytes is not None:
+            lines_all = "\r".join(lines_to_print)
+            print(type(lines_all))
+            lines_to_print = lines_all[-bytes:]
+            print(lines_to_print)
 
     if lines is not None:
-        lines_to_print = lines_to_print[-int(lines):]
+        lines_to_print = lines_to_print[-int(lines):].encode()
+    # else:
+    #     lines_to_print = lines_to_print[-int(lines):]
 
-    for line in lines_to_print:
-        sys.stdout.buffer.write(line)
+    # sys.stdout.buffer.write(lines_to_print.encode())
+    # for line in lines_to_print:
+    if bytes is None:
+        print(bytes)
+        sys.stdout.buffer.write(lines_to_print)
+    else:
+        sys.stdout.buffer.write(lines_to_print.encode())
+
 
 
 class SimpleArgumentParser:
     def __init__(self):
         self.args = {}
 
-    def add_argument(self, arg, nargs=1):
-        self.args[arg] = nargs
-
-    def parse_args(self, argv):
-        parsed_args = {}
-        i = 1
-        while i < len(argv):
-            if argv[i] in self.args:
-                nargs = self.args[argv[i]]
-                if nargs == 1:
-                    parsed_args[argv[i]] = argv[i + 1]
-                    i += 2
+    def add_argument(self, argv):
+        for i in range(1, len(argv)):
+            print("arg: " + argv[i])
+            if argv[i] != "<" and argv[i] != "type":
+                if argv[i].startswith("--lines"):
+                    self.args["lines"] = int(argv[i][8:])
+                elif argv[i].startswith("--bytes"):
+                    self.args["bytes"] = int(argv[i][8:])
                 else:
-                    parsed_args[argv[i]] = argv[i + 1:i + 1 + nargs]
-                    i += 1 + nargs
-            else:
-                i += 1
-        return parsed_args
+                    self.args["file"] = argv[i]
+
+    # def parse_args(self):
+    #     parsed_args = {}
+    #     i = 1
+    #     while i < len(argv):
+    #         if argv[i][:7] in self.args:
+    #             nargs = self.args[argv[i][:7]]
+    #             if nargs == 1:
+    #                 parsed_args[argv[i]] = argv[i + 1]
+    #                 i += 2
+    #             else:
+    #                 parsed_args[argv[i]] = argv[i + 1:i + 1 + nargs]
+    #                 i += 1 + nargs
+    #         else:
+    #             parsed_args["file"] = argv[i]
+    #             i += 1
+    #     return parsed_args
 
 
 if __name__ == "__main__":
     parser = SimpleArgumentParser()
-    parser.add_argument("--lines", nargs=1)
-    parser.add_argument("--bytes", nargs=1)
-    parser.add_argument("file", nargs="?")
+    parser.add_argument(sys.argv)
+    # nargs=1 oznacza jeden argument do "--lines" nargs = '?' oznacza 0 lub 1 argument
+    # parser.add_argument("--lines", nargs=1)
+    # parser.add_argument("--bytes", nargs=1)
+    # parser.add_argument("file", nargs="?")
 
-    arg_num = len(sys.argv)
-    print("arg_num= ", arg_num)
-    for i in range(arg_num):
-        print(sys.argv[i])
-    args = parser.parse_args(sys.argv)
-
-    print(args)
+    # print(parser)
+    # arg_num = len(sys.argv)
+    # print("arg_num= ", arg_num)
+    # for i in range(arg_num):
+    #     print(sys.argv[i])
+    # args = parser.parse_args()
+    #
+    # print(args)
 
     # parser = argparse.ArgumentParser(description="Print the last lines of a file or stdin.")
     # parser.add_argument("file", nargs="?", help="File to read from.")
@@ -76,7 +111,8 @@ if __name__ == "__main__":
     # python lab_4_3tail.py test.txt
     # cat inny_plik.txt | python lab_4_3tail.py test.txt
     #
-    tail(args.file, args.lines, args.bytes)
+    # tail(args.get("file"), args.lines, args.bytes)
+    tail(parser.args.get("file"), parser.args.get("lines"), parser.args.get("bytes"))
     #
     # logs = read_log()
     # # testowy wydruk     python lab_4_3tail.py < test.txt
